95c8b6872b1c27d88cc1680c326753d3a11bdda6
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-06-01 02:16:42 +0530

    DAY 6

---
+++ DAY6/functions.py
@@ -0,0 +1,18 @@
+#Built in Fuctions
+print("hello")
+x=len("hello")
+print(x)
+
+#defining Fuction
+# def Name():
+#     do this
+#     then do this 
+#     finally do this
+
+#Making a Fuction
+def myFuction():
+    print("hello")
+    print("Bye")
+
+#calling a function
+myFuction()
---
+++ DAY6/whileLoop.py
@@ -0,0 +1,11 @@
+num=100
+while num>0:
+    print(num)
+    num-=1
+
+# while something_is_true:
+#     do this
+#     then do this
+while num!=100:#not equal to
+    print(num)
+    num+=2
