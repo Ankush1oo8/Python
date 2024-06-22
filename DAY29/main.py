be41972f9a25d2ffd202b6b93b96939744a9b8f4
Ankush1oo8 <ankushchudiwal@outlook.com>
2024-06-22 17:33:23 +0530

    password generator

--- DAY29/main.py
+++ DAY29/main.py
@@ -34,7 +34,7 @@ def save():
     else:
         is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\nPassword: {password} \nIs it ok to save?")
         if is_ok:
-            with open("data.txt", "a") as data_file:
+            with open("DAY29/data.txt", "a") as data_file:
                 data_file.write(f"{website} | {email} | {password}\n")
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
@@ -73,31 +73,6 @@ password_entry.grid(row=3, column=1)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
-
-window.mainloop()
-#Labels
-website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
-email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
-password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
-
-#Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
-website_entry.focus()
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
-password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
-
-# Buttons
-generate_password_button = Button(text="Generate Password")
-generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
